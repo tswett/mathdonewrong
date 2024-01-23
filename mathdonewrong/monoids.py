dbabcb0b5d3edcf246260bee17d7d6099234a4fa
Tanner Swett <tannerswett@gmail.com>
2024-01-23 17:11:51 -0500

    Start implementing the category of tuple types

--- mathdonewrong/monoids.py
+++ mathdonewrong/monoids.py
@@ -10,6 +10,8 @@
 
 from __future__ import annotations
 from functools import reduce
+import inspect
+from typing import Any, Callable, TypeVar
 
 class Monoid:
     T: type
@@ -63,10 +65,37 @@ class StringMonoid(AdditiveMonoid):
 string_monoid = StringMonoid()
 
 class TupleMonoid(AdditiveMonoid):
-    T = tuple
+    def __init__(self, T: type):
+        self.T = tuple[T, ...]
 
     @property
-    def id(self) -> tuple:
+    def id(self) -> tuple[T, ...]:
         return ()
 
-tuple_monoid = TupleMonoid()
+tuple_monoid = TupleMonoid(Any)
+
+class MonoidHomomorphism:
+    def __init__(self, f: Callable, domain: Monoid = None, codomain: Monoid = None):
+        if domain is None:
+            first_parameter_name = list(inspect.signature(f).parameters)[0]
+            domain = inspect.get_annotations(f, eval_str=True)[first_parameter_name]
+        if codomain is None:
+            codomain = inspect.get_annotations(f, eval_str=True)['return']
+
+        self.domain = domain
+        self.codomain = codomain
+        self.f = f
+
+    def __call__(self, x: domain.T) -> codomain.T:
+        return self.f(x)
+
+@MonoidHomomorphism
+def string_length(x: string_monoid) -> int_addition:
+    return len(x)
+
+def int_scale(x: int) -> MonoidHomomorphism:
+    @MonoidHomomorphism
+    def f(y: int_addition) -> int_addition:
+        return x * y
+
+    return f
--- test_monoids.py
+++ test_monoids.py
@@ -8,7 +8,8 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See version 3 of the GNU GPL for more details.
 
-from mathdonewrong.monoids import int_addition, int_multiplication, string_monoid, tuple_monoid
+import pytest
+from mathdonewrong.monoids import MonoidHomomorphism, int_addition, int_multiplication, int_scale, string_monoid, tuple_monoid
 
 def test_int_addition():
     assert int_addition.oper() == 0
@@ -32,3 +33,26 @@ def test_tuple_monoid():
     assert tuple_monoid.oper() == ()
     assert tuple_monoid.oper((1, 'hello')) == (1, 'hello')
     assert tuple_monoid.oper((1, 'hello'), (), ('world',)) == (1, 'hello', 'world')
+
+def test_a_homomorphism():
+    @MonoidHomomorphism
+    def int_to_unary(x: int_addition) -> string_monoid:
+        return '.' * x
+
+    assert int_to_unary.domain == int_addition
+    assert int_to_unary.codomain == string_monoid
+
+    assert int_to_unary(6) == '......'
+
+def test_int_scale_homomorphism():
+    for x in [1, 5, 0, -1]:
+        hom = int_scale(x)
+
+        assert hom.domain == int_addition
+        assert hom.codomain == int_addition
+
+        for y in [1, 5, 0, -1]:
+            assert hom(y) == x * y
+
+if __name__ == '__main__':
+    pytest.main([__file__])
