17d56972a38aecd2eb943f236ada761e402e5046
Tanner Swett <tannerswett@gmail.com>
2024-02-06 20:48:39 -0500

    More expression parsing

--- mathdonewrong/python_exprs/python_exprs.py
+++ mathdonewrong/python_exprs/python_exprs.py
@@ -10,6 +10,7 @@
 
 from ast import NodeVisitor
 import ast
+from typing import Any
 
 from mathdonewrong.expressions import Expression, Literal, NamedOper
 
@@ -28,24 +29,76 @@ class PyAttr(PythonExpr, NamedOper):
 def PyAttrL(target, name):
     return PyAttr(target, Literal(name))
 
+class PyCall(PythonExpr, NamedOper):
+    pass
+
+class PyArgs(PythonExpr, NamedOper):
+    pass
+
+def PyCallL(target, *args):
+    return PyCall(target, PyArgs(args))
+
+class PyReturn(PythonExpr, NamedOper):
+    pass
+
+class PyBlock(PythonExpr, NamedOper):
+    pass
+
+class PyFunctionDef(PythonExpr, NamedOper):
+    pass
+
+class PyDecorators(PythonExpr, NamedOper):
+    pass
+
+def PyFunctionDefL(decorators, name, args, body):
+    return PyFunctionDef(
+        PyDecorators(*decorators),
+        Literal(name),
+        PyArgs(*args),
+        PyBlock(*body))
+
 class ExpressionizeNodeVisitor(NodeVisitor):
     def generic_visit(self, node):
         raise NotImplementedError(f"Node type {type(node).__name__} not implemented")
+
+    def visit_arg(self, node: ast.arg) -> PythonExpr:
+        return PyNameL(node.arg)
 
     def visit_Attribute(self, node: ast.Attribute) -> PythonExpr:
         target = self.visit(node.value)
         return PyAttrL(target, node.attr)
 
+    def visit_Call(self, node: ast.Call) -> PythonExpr:
+        target = self.visit(node.func)
+        args = [self.visit(arg) for arg in node.args]
+        return PyCallL(target, *args)
+
+    def visit_Expr(self, node: ast.Expr) -> PythonExpr:
+        return self.visit(node.value)
+
     def visit_Expression(self, node: ast.Expression) -> PythonExpr:
         return self.visit(node.body)
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> PythonExpr:
+        decorators = [self.visit(decorator) for decorator in node.decorator_list]
+        args = [self.visit(arg) for arg in node.args.args]
+        body = [self.visit(statement) for statement in node.body]
+        return PyFunctionDefL(decorators, node.name, args, body)
+
+    def visit_Module(self, node: ast.Module) -> PythonExpr:
+        statements = [self.visit(statement) for statement in node.body]
+        return PyBlock(*statements)
 
     def visit_Name(self, node: ast.Name) -> PythonExpr:
         return PyNameL(node.id)
 
+    def visit_Return(self, node: ast.Return) -> PythonExpr:
+        return PyReturn(self.visit(node.value))
+
 def source_expr_to_expr(source):
-    # Parse "source" as an expression
     tree = ast.parse(source, mode='eval')
     return ExpressionizeNodeVisitor().visit(tree)
 
-def source_module_to_expr(source):
-    pass
+def source_block_to_expr(source):
+    tree = ast.parse(source)
+    return ExpressionizeNodeVisitor().visit(tree)
--- test/test_python_exprs.py
+++ test/test_python_exprs.py
@@ -8,22 +8,64 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See version 3 of the GNU GPL for more details.
 
+import textwrap
 import pytest
 
-from mathdonewrong.python_exprs.python_exprs import PyAttrL, PyNameL, source_expr_to_expr, source_module_to_expr
-
-relation_def = '''
-    @relation()
-    def assoc(self, x, y, z):
-        return self.mult(self.mult(x, y), z)
-'''
+from mathdonewrong.python_exprs.python_exprs import PyAttrL, PyBlock, PyCallL, PyFunctionDefL, PyNameL, PyReturn, source_expr_to_expr, source_block_to_expr
 
 def test_parse_an_expression():
     assert source_expr_to_expr('x') == PyNameL('x')
     assert source_expr_to_expr('self.mult') == PyAttrL(PyNameL('self'), 'mult')
+    assert source_expr_to_expr('f(x, y)') == PyCallL(PyNameL('f'), PyNameL('x'), PyNameL('y'))
+
+def test_parse_a_block():
+    assert source_block_to_expr('return x') == PyBlock(PyReturn(PyNameL('x')))
+
+    assert source_block_to_expr('f(x)\nreturn x') == PyBlock(
+        PyCallL(PyNameL('f'), PyNameL('x')),
+        PyReturn(PyNameL('x'))
+    )
+
+    function_def = textwrap.dedent('''\
+        def f(x):
+            return x''')
+    assert source_block_to_expr(function_def) == PyBlock(
+        PyFunctionDefL(
+            [],
+            'f',
+            [PyNameL('x')],
+            [PyReturn(PyNameL('x'))]))
+
+    decorated_function_def = textwrap.dedent('''\
+        @deco
+        def f(x):
+            return x''')
+    assert source_block_to_expr(decorated_function_def) == PyBlock(
+        PyFunctionDefL(
+            [PyNameL('deco')],
+            'f',
+            [PyNameL('x')],
+            [PyReturn(PyNameL('x'))]))
+
+relation_def = textwrap.dedent('''\
+    @relation()
+    def assoc(self, x, y, z):
+        return self.mult(self.mult(x, y), z)''')
 
 def test_parse_a_relation_def():
-    source_module_to_expr(relation_def)
+    assert source_block_to_expr(relation_def) == PyBlock(
+        PyFunctionDefL(
+            [PyCallL(PyNameL('relation'))],
+            'assoc',
+            [PyNameL('self'), PyNameL('x'), PyNameL('y'), PyNameL('z')],
+            [PyReturn(
+                PyCallL(
+                    PyAttrL(PyNameL('self'), 'mult'),
+                    PyCallL(
+                        PyAttrL(PyNameL('self'), 'mult'),
+                        PyNameL('x'),
+                        PyNameL('y')),
+                    PyNameL('z')))]))
 
 if __name__ == '__main__':
     pytest.main([__file__])
