d33ee2188484043a0a2653d47a56773a91756c34
Tanner Swett <tannerswett@gmail.com>
2024-02-15 21:25:41 -0500

    More typed primitive recursive arithmetic...

--- mathdonewrong/primitive_recursive/primrec_exprs_typed.py
+++ mathdonewrong/primitive_recursive/primrec_exprs_typed.py
@@ -8,10 +8,12 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See version 3 of the GNU GPL for more details.
 
+from __future__ import annotations
+
 from typing import Callable, ParamSpec, TypeVar
 
 from mathdonewrong.algebras import Algebra
-from mathdonewrong.expressions import Expression, NamedOper
+from mathdonewrong.expressions import Expression, Literal, NamedOper
 
 P = ParamSpec('P')
 T, U, V = TypeVar('T'), TypeVar('U'), TypeVar('V')
@@ -29,8 +31,27 @@ class Succ(PrimRecExpr, NamedOper):
 class Zero(PrimRecExpr, NamedOper):
     pass
 
+class Fork(PrimRecExpr, NamedOper):
+    pass
+
+class Select(PrimRecExpr, NamedOper):
+    def __init__(self, index: int, first: type, second: type):
+        super().__init__(Literal(index), Literal(first), Literal(second))
+
+    def __repr__(self):
+        index, first, second = self.operands
+        return f'Select({index.value}, {first.value}, {second.value})'
+
 class Comp(PrimRecExpr, NamedOper):
     pass
+
+class Const(PrimRecExpr, NamedOper):
+    def __init__(self, domain: type, codomain: type, value: codomain):
+        super().__init__(Literal(domain), Literal(codomain), Literal(value))
+
+    def __repr__(self):
+        domain, codomain, value = self.operands
+        return f'Const({domain.value}, {codomain.value}, {value.value})'
 
 class TypecheckAlgebra(Algebra):
     def succ(self):
@@ -39,15 +60,31 @@ class TypecheckAlgebra(Algebra):
     def zero(self):
         return Callable[[], int]
 
+    def fork(self, first: type, second: type) -> type:
+        first_args, first_result = first.__args__[:-1], first.__args__[-1]
+        second_args, second_result = second.__args__[:-1], second.__args__[-1]
+
+        if first_args == second_args:
+            return Callable[first_args, tuple[first_result, second_result]]
+        else:
+            return None
+
+    def select(self, index: int, first: type, second: type) -> type:
+        result = [first, second][index]
+
+        return Callable[[tuple[first, second]], result]
+
     def comp(self, first: type, second: type) -> type:
         first_args, first_result = first.__args__[:-1], first.__args__[-1]
         second_args, second_result = second.__args__[:-1], second.__args__[-1]
         
         if second_args == (first_result,):
-            # TODO: return the right thing
-            return first
+            return Callable[first_args, second_result]
         else:
             return None
+
+    def const(self, domain: type, codomain: type, value: codomain) -> type:
+        return Callable[[domain], codomain]
 
 class ToFuncAlgebra(Algebra):
     def succ(self):
@@ -62,8 +99,26 @@ class ToFuncAlgebra(Algebra):
 
         return zero_func
 
+    def fork(self, first: Callable[P, T], second: Callable[P, U]) -> Callable[P, tuple[T, U]]:
+        def fork_func(*args: P.args) -> tuple[T, U]:
+            return (first(*args), second(*args))
+
+        return fork_func
+
+    def select(self, index, first, second):
+        def select_func(t):
+            return t[index]
+
+        return select_func
+
     def comp(self, first: Callable[P, U], second: Callable[[U], V]) -> Callable[P, V]:
         def comp_func(*args: P.args) -> V:
             return second(first(*args))
 
         return comp_func
+
+    def const(self, domain: type, codomain: type, value: codomain) -> Callable[[domain], codomain]:
+        def const_func(x: domain) -> codomain:
+            return value
+
+        return const_func
--- test/test_primitive_recursive_typed.py
+++ test/test_primitive_recursive_typed.py
@@ -10,7 +10,9 @@
 
 from typing import Callable
 
-from mathdonewrong.primitive_recursive.primrec_exprs_typed import Comp, Succ, Zero
+import pytest
+
+from mathdonewrong.primitive_recursive.primrec_exprs_typed import Comp, Const, Fork, Select, Succ, Zero
 
 def test_succ():
     assert Succ().typecheck() == Callable[[int], int]
@@ -21,6 +23,29 @@ def test_succ():
 def test_zero():
     assert Zero().typecheck() == Callable[[], int]
     assert Zero().to_func()() == 0
+
+def test_fork():
+    assert Fork(Zero(), Zero()).typecheck() == Callable[[], tuple[int, int]]
+    assert Fork(Zero(), Zero()).to_func()() == (0, 0)
+
+    assert Fork(Succ(), Succ()).typecheck() == Callable[[int], tuple[int, int]]
+    assert Fork(Succ(), Succ()).to_func()(0) == (1, 1)
+    assert Fork(Succ(), Succ()).to_func()(1) == (2, 2)
+
+    assert Fork(Succ(), Fork(Succ(), Succ())).typecheck() == Callable[[int], tuple[int, tuple[int, int]]]
+    assert Fork(Succ(), Fork(Succ(), Succ())).to_func()(0) == (1, (1, 1))
+    assert Fork(Succ(), Fork(Succ(), Succ())).to_func()(1) == (2, (2, 2))
+
+    assert Fork(Zero(), Succ()).typecheck() == None
+
+def test_select():
+    assert repr(Select(0, int, tuple[int, int])) == "Select(0, <class 'int'>, tuple[int, int])"
+
+    assert Select(0, int, tuple[int, int]).typecheck() == Callable[[tuple[int, tuple[int, int]]], int]
+    assert Select(0, int, tuple[int, int]).to_func()((0, (1, 2))) == 0
+
+    assert Select(1, int, tuple[int, int]).typecheck() == Callable[[tuple[int, tuple[int, int]]], tuple[int, int]]
+    assert Select(1, int, tuple[int, int]).to_func()((0, (1, 2))) == (1, 2)
 
 def test_comp():
     # Note: this is left-to-right composition
@@ -34,3 +59,33 @@ def test_comp():
     assert Comp(Succ(), Comp(Succ(), Succ())).to_func()(0) == 3
 
     assert Comp(Succ(), Zero()).typecheck() == None
+
+    assert Comp(Succ(), Fork(Succ(), Succ())).typecheck() == Callable[[int], tuple[int, int]]
+    assert Comp(Succ(), Fork(Succ(), Succ())).to_func()(0) == (2, 2)
+    assert Comp(Succ(), Fork(Succ(), Succ())).to_func()(1) == (3, 3)
+
+def test_const():
+    assert repr(Const(None, int, 10)) == "Const(None, <class 'int'>, 10)"
+
+    assert Const(None, int, 10).typecheck() == Callable[[None], int]
+    assert Const(None, int, 10).to_func()(None) == 10
+
+    assert Const(int, None, None).typecheck() == Callable[[int], None]
+    assert Const(int, None, None).to_func()(18) == None
+
+@pytest.mark.skip("Not implemented yet")
+def test_natrecurse():
+    # Double-plus-ten
+
+    base = Const(None, int, 10)
+    assert base.typecheck() == Callable[[None], int]
+
+    step = Select(0, None, int)
+    assert step.typecheck() == Callable[[tuple[None, int]], int]
+
+    double_plus_ten = NatRecurse(base, step)
+    assert double_plus_ten.typecheck() == Callable[[tuple[None, int]], int]
+
+    assert double_plus_ten.to_func()((None, 0)) == 10
+    assert double_plus_ten.to_func()((None, 1)) == 12
+    assert double_plus_ten.to_func()((None, 2)) == 14
