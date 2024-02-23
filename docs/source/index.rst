861a08b30e5c15b1bc8e2f3b6bb1094b34d28e68
Tanner Swett <tannerswett@gmail.com>
2024-02-23 18:28:34 -0500

    Write some more scraps of documentation

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -9,12 +9,43 @@
    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
    FOR A PARTICULAR PURPOSE. See version 3 of the GNU GPL for more details.
 
-Welcome to mathdonewrong's documentation!
-=========================================
+Math done wrong!
+================
 
 This is "mathdonewrong," a somewhat silly attempt at doing formalized
 mathematics in Python. You may be thinking that Python is not the right language
 for doing formalized mathematics in—and that's the reason for the name.
+
+The two fundamental classes are :class:`~mathdonewrong.expressions.Expression`
+and :class:`~mathdonewrong.algebras.Algebra`.
+
+An ``Expression`` is an expression tree, consisting of operators, literals, and
+variables. Generally speaking, an expression, by itself, is meaningless. An
+``Algebra`` is an object that provides meanings for expressions by implementing
+their operators.
+
+There are many subclasses of ``Algebra`` corresponding to various varieties and
+instances of mathematical structures. For example,
+:class:`~mathdonewrong.monoids.Monoid` is a subclass of ``Algebra`` which
+describes the concept of a monoid (but doesn't provide any implementations), and
+:class:`~mathdonewrong.monoids.IntAddition` and
+:class:`~mathdonewrong.monoids.IntMultiplication` are subclasses of ``Monoid``
+which actually represent particular monoids, and provide the appropriate
+implementations.
+
+There are a few modules which attempt to implement various particular kinds of
+mathematical structures. In no particular order, some of these are:
+
+- The :mod:`~mathdonewrong.monoids` module describes monoids and implements a
+  few examples. It also contains an initial attempt at defining what a
+  homomorphism is.
+- The :mod:`~mathdonewrong.boolean_algebra` module describes Boolean algebras
+  and provides an implementation of the standard Boolean algebra.
+- The ``categories`` (TBI) and :mod:`~mathdonewrong.monoidal_categories` modules
+  are an early-feeling attempt at defining categories.
+- The :mod:`~mathdonewrong.lambda_calc` and
+  :mod:`~mathdonewrong.primitive_recursive` modules define a couple of models of
+  computation.
 
 (sorry, I don't know how Sphinx works so this documentation looks pretty messed
 up at the moment)
--- mathdonewrong/common/common_opers.py
+++ mathdonewrong/common/common_opers.py
@@ -14,23 +14,26 @@ class Compose(NamedOper):
     r""""End-to-end" composition
 
     This operation corresponds to composition of morphisms in a category.
-    Generally, if `f` is something of type :math:`A \to B` and `g` is something
-    of type :math:`B \to C`, then `Compose(f, g)` is something of type :math:`A
-    \to C`.
+    Generally, if ``f`` is something of type :math:`A \to B` and ``g`` is
+    something of type :math:`B \to C`, then ``Compose(f, g)`` is something of
+    type :math:`A \to C`.
 
-    A good Python operator to use for `Compose` may be the `@` operator, since
+    A good Python operator to use for ``Compose`` may be the ``@`` operator, since
     that operator traditionally represents matrix multiplication, and matrix
-    multiplication is composition.
+    multiplication can be thought of as a kind of composition.
+
+    Note that, at least for functions, this is left-to-right composition—the
+    first function goes first and the second function goes second.
 
     "Compose" is one of many plausible names for the sole operator of a monoid.
     """
 
 class Stack(NamedOper):
-    r""""Side-by-side" stacking
+    r""""Side-by-side" composition
     
     This operation corresponds to the monoidal product in a monoidal category.
-    Generally, if `f` is something of type :math:`A \to C` and `g` is something
-    of type :math:`B \to D`, then `Stack(f, g)` is something of type :math:`(A
+    Generally, if ``f`` is something of type :math:`A \to C` and ``g`` is something
+    of type :math:`B \to D`, then ``Stack(f, g)`` is something of type :math:`(A
     \otimes B) \to (C \otimes D)`.
 
     "Stack" is one of many plausible names for the sole operator of a monoid.
@@ -41,7 +44,7 @@ class Plus(NamedOper):
 
     This operation corresponds to addition in a ring.
 
-    The obvious Python operator to use for `Plus` is the `+` operator.
+    The obvious Python operator to use for ``Plus`` is the ``+`` operator.
 
     "Plus" is one of many plausible names for the sole operator of a monoid.
     """
@@ -57,7 +60,7 @@ class Times(NamedOper):
 
     This operation corresponds to multiplication in a ring.
 
-    The obvious Python operator to use for `Times` is the `*` operator.
+    The obvious Python operator to use for ``Times`` is the ``*`` operator.
 
     "Times" is one of many plausible names for the sole operator of a monoid.
     """
@@ -74,13 +77,13 @@ class Min(NamedOper):
     This operation corresponds to the minimum or meet operation in a lattice, or
     the conjunction operation in a Boolean algebra.
 
-    The best Python operator to use for `Min` is probably the `&` operator;
-    booleans and sets in Python both use the `&` operator that way. It's worth
-    noting that ints in Python use `&` for bitwise AND, not for the minimum.
+    The best Python operator to use for ``Min`` is probably the ``&`` operator;
+    booleans and sets in Python both use the ``&`` operator that way. It's worth
+    noting that ints in Python use ``&`` for bitwise AND, not for the minimum.
 
-    It is generally impossible to use Python's `and` operator to represent this
+    It is generally impossible to use Python's ``and`` operator to represent this
     operator, since Python doesn't give us very much flexibility in customizing
-    what the `and` operator does.
+    what the ``and`` operator does.
 
     "Min" is one of many plausible names for the sole operator of a monoid.
     """
@@ -98,13 +101,13 @@ class Max(NamedOper):
     This operation corresponds to the maximum or join operation in a lattice, or
     the disjunction operation in a Boolean algebra.
 
-    The best Python operator to use for `Max` is probably the `|` operator;
-    booleans and sets in Python both use the `|` operator that way. It's worth
-    noting that ints in Python use `|` for bitwise OR, not for the maximum.
+    The best Python operator to use for ``Max`` is probably the ``|`` operator;
+    booleans and sets in Python both use the ``|`` operator that way. It's worth
+    noting that ints in Python use ``|`` for bitwise OR, not for the maximum.
 
-    It is generally impossible to use Python's `or` operator to represent this
+    It is generally impossible to use Python's ``or`` operator to represent this
     operator, since Python doesn't give us very much flexibility in customizing
-    what the `or` operator does.
+    what the ``or`` operator does.
 
     "Max" is one of many plausible names for the sole operator of a monoid.
     """
